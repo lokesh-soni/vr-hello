025cfc027049f31ebebf42a263de9f4fe216c096
Lokesh Soni <lokeshsoni04@gmail.com>
2018-02-28 07:23:31 +0530

    videoPano

--- index.vr.js
+++ index.vr.js
@@ -4,6 +4,7 @@ import {
   asset,
   Pano,
   Text,
+  VideoPano,
   View,
 } from 'react-vr';
 
@@ -11,21 +12,7 @@ export default class hello extends React.Component {
   render() {
     return (
       <View>
-        <Pano source={asset('chess-world.jpg')}/>
-        <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          hello
-        </Text>
+        <VideoPano source={{uri: 'vr.mp4'}} />
       </View>
     );
   }
